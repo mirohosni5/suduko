06498157b13d646c42d4e4f8b7db9014a839b5c8
mayar <mirohosni5@gmail.com>
2025-11-29 00:49:08 +0200

    auto: MAYAR 2025-11-29T00:49:07.9567123+02:00

---
+++ src/checker/RowChecker.java
@@ -0,0 +1,31 @@
+package checker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RowChecker {
+
+    public static List<String> check(int[][] board) {
+        List<String> errors = new ArrayList<>();
+
+        for (int r = 0; r < 9; r++) {
+            for (int c1 = 0; c1 < 9; c1++) {
+                int v1 = board[r][c1];
+                if (v1 == 0) continue;
+                if (v1 < 1 || v1 > 9) {
+                    errors.add("Row " + (r + 1) + " has a wrong number (" + v1 + ") at column " + (c1 + 1));
+                    continue;
+                }
+
+                for (int c2 = c1 + 1; c2 < 9; c2++) {
+                    int v2 = board[r][c2];
+                    if (v2 == v1) {
+                        errors.add("Row " + (r + 1) + " repeats the number " + v1);
+                    }
+                }
+            }
+        }
+
+        return errors;
+    }
+}
---
+++ src/checker/SequentialMode.java
@@ -0,0 +1,30 @@
+package checker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SequentialMode {
+
+    public static List<String> validate(int[][] board) {
+        List<String> all = new ArrayList<>();
+        all.addAll(RowChecker.check(board));
+        all.addAll(ColumnChecker.check(board));
+        all.addAll(BoxChecker.check(board));
+        return all;
+    }
+
+    public static void printResult(int[][] board) {
+        List<String> errors = validate(board);
+        if (errors.isEmpty()) {
+            System.out.println("VALID");
+        } else {
+            System.out.println("INVALID");
+            for (String e : errors) {
+                System.out.println(e);
+            }
+        }
+    }
+
+
+}
+
