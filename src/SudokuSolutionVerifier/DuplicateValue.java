b6b27469a7dffa9053b1ea76ef7b167776bf9ac2
Rana Mohamed <es.Rana.Mohamed2023@alexu.edu.eg>
2025-11-29 02:18:15 +0200

    DuplicateValue Class

---
+++ src/SudokuSolutionVerifier/DuplicateValue.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SudokuSolutionVerifier;
+
+import java.util.List;
+
+/**
+ *
+ * @author M
+ */
+public class DuplicateValue {
+     private int value;
+    private List<Integer> positions;
+
+    public DuplicateValue(int value, List<Integer> positions) {
+        this.value = value;
+        this.positions = positions;
+    }
+    
+}
