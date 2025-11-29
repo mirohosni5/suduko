e2050fbce0dee817cd363e3823d105a386f0b648
Rana Mohamed <es.Rana.Mohamed2023@alexu.edu.eg>
2025-11-29 02:28:29 +0200

    CSVReader class

--- src/SudokuSolutionVerifier/CSVReader.java
+++ src/SudokuSolutionVerifier/CSVReader.java
@@ -5,6 +5,7 @@
 package SudokuSolutionVerifier;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -23,25 +24,34 @@ public class CSVReader {
         }
         return reader;
     }
-     public int[][] readCSV(String filePath) {
+     public int[][] readCSV(String filePath) throws FileNotFoundException, IOException {
         int[][] board = new int[9][9];
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             int row = 0;
             while ((line = br.readLine()) != null && row < 9) {
-                String[] values = line.split(",");
-                for (int col = 0; col < 9 && col < values.length; col++) {
-                    board[row][col] = Integer.parseInt(values[col].trim());
+                line = line.trim();
+                if (line.isEmpty()) continue;
+                String[] values ;
+                 if (line.contains(",")) {
+                    values= line.split(",");
+                } else {
+                    values= line.split("\\s+");
                 }
+                 if (values.length != 9) {
+                    throw new IllegalArgumentException("Each row must have only 9 values.");
+                }
+            for (int col=0;col<9;col++) {
+                try {
+                    int  i = Integer.parseInt(values[col].trim()); 
+                    if (i < 1 || i > 9) throw new NumberFormatException(); 
+                        board[row][col] = i; 
+                    } catch (NumberFormatException ex) {
+                        throw new IllegalArgumentException("Invalid number at row " + (row+1) + " col " + (col+1));
+                    }}
                 row++;
             }
-        } catch (IOException e) {
-            System.out.println("cannot open the file");
-            return null;
+            if (row != 9) throw new IllegalArgumentException("File must contain 9 rows.Found " + row +"rows.");
         }
         return board;
-    }
-}
-    
-    
-
+    }}
