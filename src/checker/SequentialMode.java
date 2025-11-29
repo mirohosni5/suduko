a4c76f8021267561bd19df639bfaee517fa2526f
mayar <mirohosni5@gmail.com>
2025-11-29 03:15:24 +0200

    auto: MAYAR 2025-11-29T03:15:24.5128001+02:00

--- src/checker/SequentialMode.java
+++ src/checker/SequentialMode.java
@@ -1,2 +1,25 @@
 package checker;
 
+package mode0;
+
+import java.util.List;
+
+public class SequentialMode extends BasicChecks {
+
+    public SequentialMode(int[][] board) {
+        super(board);
+    }
+
+    public void printResult() {
+        List<String> errors = checkAll();
+
+        if (errors.isEmpty()) {
+            System.out.println("VALID");
+        } else {
+            System.out.println("INVALID");
+            for (String e : errors) {
+                System.out.println(e);
+            }
+        }
+    }
+}
