f1b4a0477f2a4c549a96d46fa21e52c7774413dd
Aya-Gebril <aya2gebril@gmail.com>
2025-11-30 01:13:16 +0200

    TwentySevenThreadMode run method+

--- src/SudokuSolutionVerifier/TwentySevenThreadMode.java
+++ src/SudokuSolutionVerifier/TwentySevenThreadMode.java
@@ -19,13 +19,19 @@ public class TwentySevenThreadMode implements SudokuMode {
     }
 
     @Override
-    public ValidationResult run(){
+    public ValidationResult run() {
         List<String> errors = Collections.synchronizedList(new ArrayList<>());
         List<Thread> threads = new ArrayList<>();
 
         // 9 Row threads
         for (int i = 0; i < 9; i++) {
             Thread t = new Thread(new SingleRowThread(i, board, errors));
+            threads.add(t);
+            t.start();
+        }
+        // 9 Column threads
+        for (int i = 0; i < 9; i++) {
+            Thread t = new Thread(new SingleColumnThread(i, board, errors));
             threads.add(t);
             t.start();
         }
