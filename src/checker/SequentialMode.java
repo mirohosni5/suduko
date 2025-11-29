32a63364b599a3a10bd891d13fe99cc56bf7b40b
mayar <mirohosni5@gmail.com>
2025-11-29 03:41:57 +0200

    auto: MAYAR 2025-11-29T03:41:57.6349527+02:00

--- src/checker/SequentialMode.java
+++ src/checker/SequentialMode.java
@@ -1,17 +1,25 @@
 package checker;
 
-
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SequentialMode extends BasicChecks {
 
-    public SequentialMode(int[][] board) {
-        super(board);
+    public SequentialMode() {
+        super(null);   // we will set the board later using setBoard()
+    }
+
+    public void setBoard(int[][] b) {
+        this.board = b;
+    }
+
+    @Override
+    public List<String> validate() {
+        return checkAll();
     }
 
     public void printResult() {
-        List<String> errors = checkAll();
+        List<String> errors = validate();
 
         if (errors.isEmpty()) {
             System.out.println("VALID");
