132d522e7075894cd342d9c351642068b34d1d5d
Aya-Gebril <aya2gebril@gmail.com>
2025-11-30 00:28:26 +0200

    SingleColumnThread attr. & const.

--- src/SudokuSolutionVerifier/SingleColumnThread.java
+++ src/SudokuSolutionVerifier/SingleColumnThread.java
@@ -1,4 +1,20 @@
 package SudokuSolutionVerifier;
 
-public class SingleColumnThread {
+import java.util.List;
+
+public class SingleColumnThread implements Runnable {
+    private int colIndex;
+    private int[][] board;
+    private List<String> errors;
+
+    public SingleColumnThread(int colIndex, int[][] board, List<String> errors) {
+        this.colIndex = colIndex;
+        this.board = board;
+        this.errors = errors;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
