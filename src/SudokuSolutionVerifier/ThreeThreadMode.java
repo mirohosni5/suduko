1cbb999383fced2f4136b19fec7bc703e860a339
Enjyzaghloul <es.engy.Zaghlol2023@alexu.edu.eg>
2025-11-29 04:47:09 +0200

    three

--- src/SudokuSolutionVerifier/ThreeThreadMode.java
+++ src/SudokuSolutionVerifier/ThreeThreadMode.java
@@ -4,54 +4,52 @@
  */
 package SudokuSolutionVerifier;
 import checker.BasicChecks;
-import checker.SequentialMode;
+import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.*;
 /**
  *
  * @author Engyz
  */
-class ThreeThreadMode extends BasicChecks{
+//
 
-    public ThreeThreadMode(int[][] board) {
+
+class ThreeThreadMode extends BasicChecks {
+
+public ThreeThreadMode(int[][] board) {
         super(board);
     }
-    
- public ValidationResult verify(int[][] board) {
+
+public ValidationResult verify() {
         ExecutorService executor = Executors.newFixedThreadPool(3);
         List<Future<ValidationResult>> futures = new ArrayList<>();
 
        
- futures.add(executor.submit(() -> {
-            ValidationResult result = new ValidationResult();
-            for (int i = 0; i < 9; i++) {
-                checkRow(board, i, result);
-            }
-            return result;
-        }));
-
-       
 futures.add(executor.submit(() -> {
             ValidationResult result = new ValidationResult();
-            for (int i = 0; i < 9; i++) {
-                checkColumn(board, i, result);
-            }
+            checkRows().forEach(result::addError); 
             return result;
         }));
 
-     
-  futures.add(executor.submit(() -> {
-            ValidationResult result = new ValidationResult();
-            for (int i = 0; i < 9; i++) {
-                checkBox(board, i, result);
-            }
-            return result;
+        
+futures.add(executor.submit(() -> {
+   ValidationResult result = new ValidationResult();
+   checkColumns().forEach(result::addError);
+    return result;
         }));
-     
-        ValidationResult finalResult = new ValidationResult();
+
+        
+futures.add(executor.submit(() -> {
+   ValidationResult result = new ValidationResult();
+   checkBoxes().forEach(result::addError);
+    return result;
+        }));
+
+  ValidationResult finalResult = new ValidationResult();
         for (Future<ValidationResult> future : futures) {
             try {
                 finalResult.merge(future.get());
