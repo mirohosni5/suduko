041c04cea462dfb426788ce4f964bf31a7df1698
Rana Mohamed <es.Rana.Mohamed2023@alexu.edu.eg>
2025-11-29 02:35:15 +0200

    ModesFactory

--- src/SudokuSolutionVerifier/ModesFactory.java
+++ src/SudokuSolutionVerifier/ModesFactory.java
@@ -4,10 +4,24 @@
  */
 package SudokuSolutionVerifier;
 
+import checker.SequentialMode;
+
 /**
  *
  * @author M
  */
 public class ModesFactory {
+     public static SudokuMode createMode(int mode) {
+        switch (mode) {
+            case 0:
+                return new SequentialMode();
+            case 3:
+                return new ThreeThreadMode();
+            case 27:
+                return new TwentySevenThreadMode();
+            default:
+                throw new IllegalArgumentException("Invalid mode: " + mode);
+        }
+    }
     
 }
