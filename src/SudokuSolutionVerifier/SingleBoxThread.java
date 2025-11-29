dc62895e63e659812e118e2301a69c0f53b49977
Aya-Gebril <aya2gebril@gmail.com>
2025-11-30 00:34:57 +0200

    SingleBoxThread atrr. & const.

--- src/SudokuSolutionVerifier/SingleBoxThread.java
+++ src/SudokuSolutionVerifier/SingleBoxThread.java
@@ -1,4 +1,20 @@
 package SudokuSolutionVerifier;
 
-public class SingleBoxThread {
+import java.util.List;
+
+public class SingleBoxThread implements Runnable {
+    private int boxIndex;
+    private int[][] board;
+    private List<String> errors;
+
+    public SingleBoxThread(int boxIndex, int[][] board, List<String> errors) {
+        this.boxIndex = boxIndex;
+        this.board = board;
+        this.errors = errors;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
