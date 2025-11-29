51208db508caeca799a3a437e4eb31670ccd3532
Aya-Gebril <aya2gebril@gmail.com>
2025-11-30 00:16:26 +0200

    TwentySevenThreadMode attributes & constructor

--- src/SudokuSolutionVerifier/TwentySevenThreadMode.java
+++ src/SudokuSolutionVerifier/TwentySevenThreadMode.java
@@ -1,4 +1,14 @@
 package SudokuSolutionVerifier;
 
-public class TwentySevenThreadMode {
+public class TwentySevenThreadMode implements SudokuMode{
+    private int[][] board;
+
+    public TwentySevenThreadMode(int[][] board) {
+        this.board = board;
+    }
+
+    @Override
+    public ValidationResult verify(int[][] board) {
+        return null;
+    }
 }
