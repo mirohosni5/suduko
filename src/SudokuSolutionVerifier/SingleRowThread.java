63961bb54e5115a3892ba8704ff939ec5f9aa478
Aya-Gebril <aya2gebril@gmail.com>
2025-11-30 00:23:50 +0200

    SingleRowThread attributes & constructor

--- src/SudokuSolutionVerifier/SingleRowThread.java
+++ src/SudokuSolutionVerifier/SingleRowThread.java
@@ -1,4 +1,20 @@
 package SudokuSolutionVerifier;
 
-public class SingleRowThread {
+import java.util.List;
+
+public class SingleRowThread implements Runnable{
+    private int rowIndex;
+    private int[][] board;
+    private List<String> errors;
+
+    public SingleRowThread(int rowIndex, int[][] board, List<String> errors) {
+        this.rowIndex = rowIndex;
+        this.board = board;
+        this.errors = errors;
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
