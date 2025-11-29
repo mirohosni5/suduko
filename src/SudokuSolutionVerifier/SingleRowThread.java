4b3e30cc3b4c77909140c06ae6bf0a434e2db25f
Aya-Gebril <aya2gebril@gmail.com>
2025-11-30 00:26:03 +0200

    SingleRowThread run method

--- src/SudokuSolutionVerifier/SingleRowThread.java
+++ src/SudokuSolutionVerifier/SingleRowThread.java
@@ -1,8 +1,9 @@
 package SudokuSolutionVerifier;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SingleRowThread implements Runnable{
+public class SingleRowThread implements Runnable {
     private int rowIndex;
     private int[][] board;
     private List<String> errors;
@@ -15,6 +16,19 @@ public class SingleRowThread implements Runnable{
 
     @Override
     public void run() {
+        int[] row = board[rowIndex];
+        int[] freq = new int[10];
+        List<Integer> duplicates = new ArrayList<>();
 
+        for (int num : row) {
+            freq[num]++;
+            if (freq[num] == 2) duplicates.add(num);
+        }
+
+        if (!duplicates.isEmpty()) {
+            synchronized (errors) {
+                errors.add("ROW " + (rowIndex + 1) + ", duplicates: " + duplicates);
+            }
+        }
     }
 }
