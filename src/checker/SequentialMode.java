7402f4b781a7914495d08f9f0bbb89da5564f156
mayar <mirohosni5@gmail.com>
2025-12-01 00:46:30 +0200

    auto: MAYAR 2025-12-01T00:46:29.9961276+02:00

---
+++ src/checker/SequentialMode.java
@@ -0,0 +1,50 @@
+package checker;
+
+import java.util.List;
+
+import SudokuSolutionVerifier.SudokuMode;
+import SudokuSolutionVerifier.ValidationResult;
+
+public class SequentialMode extends BasicChecks implements SudokuMode {
+
+    public SequentialMode() {
+        super(null);
+    }
+
+    public void setBoard(int[][] b) {
+        this.board = b;
+    }
+
+
+    public List<String> validate() {
+        if (this.board == null) {
+            return java.util.Collections.singletonList("Board not set");
+        }
+        return checkAll();
+    }
+
+
+    public void printResult() {
+        List<String> errors = validate();
+
+        if (errors.isEmpty()) {
+            System.out.println("VALID");
+        } else {
+            System.out.println("INVALID");
+            for (String e : errors) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    @Override
+    public ValidationResult verify(int[][] board) {
+
+        setBoard(board);
+        List<String> errors = validate();
+        boolean valid = errors.isEmpty();
+
+
+        return new ValidationResult(valid, errors);
+    }
+}
