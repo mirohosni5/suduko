54c99cb53050e082b004ec6b9b22bc4579d7b1ea
mayar <mirohosni5@gmail.com>
2025-12-01 00:31:29 +0200

    auto: MAYAR 2025-12-01T00:31:29.1549405+02:00

--- src/SudokuSolutionVerifier/SequentialMode.java
+++ src/SudokuSolutionVerifier/SequentialMode.java
@@ -1,47 +1,11 @@
 package SudokuSolutionVerifier;
 
-import java.util.List;
+import java.util.*;
 
-public class SequentialMode extends BasicChecks implements SudokuMode {
+public class SequentialMode implements SudokuMode {
 
-    public SequentialMode() {
-        super(null);
-    }
-
-    public void setBoard(int[][] b) {
-        this.board = b;
-    }
-
-
-    public List<String> validate() {
-        if (this.board == null) {
-            return java.util.Collections.singletonList("Board not set");
-        }
-        return checkAll();
-    }
-
-
-    public void printResult() {
-        List<String> errors = validate();
-
-        if (errors.isEmpty()) {
-            System.out.println("VALID");
-        } else {
-            System.out.println("INVALID");
-            for (String e : errors) {
-                System.out.println(e);
-            }
-        }
-    }
-
-    @Override
     public ValidationResult verify(int[][] board) {
-
-        setBoard(board);
-        List<String> errors = validate();
-        boolean valid = errors.isEmpty();
-
-
-        return new ValidationResult(valid, errors);
+        BasicChecks ck = new BasicChecks(board);
+        return new ValidationResult(ck.checkRows(), ck.checkCols(), ck.checkBoxes());
     }
 }
