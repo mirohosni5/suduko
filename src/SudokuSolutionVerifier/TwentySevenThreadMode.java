cac282b0f23212c826fda333cf8e1c8cc8fc4c13
Aya-Gebril <aya2gebril@gmail.com>
2025-11-30 23:24:45 +0200

    update TwentySevenThreadMode

--- src/SudokuSolutionVerifier/TwentySevenThreadMode.java
+++ src/SudokuSolutionVerifier/TwentySevenThreadMode.java
@@ -18,11 +18,8 @@ public class TwentySevenThreadMode implements SudokuMode {
     @Override
     public ValidationResult verify(int[][] board) {
         BasicChecks checker = new BasicChecks(board);
-
-        // Performs all checks (rows + columns + boxes)
         List<String> errors = checker.checkAll();
 
-        // Build the result object
         boolean isValid = errors.isEmpty();
         return new ValidationResult(isValid, errors);
     }
