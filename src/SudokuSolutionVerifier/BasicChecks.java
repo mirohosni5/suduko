210ca926c2b57c0eda346b85f87e77d994f0a683
mayar <mirohosni5@gmail.com>
2025-12-01 00:48:56 +0200

    auto: MAYAR 2025-12-01T00:48:56.6318769+02:00

--- src/SudokuSolutionVerifier/BasicChecks.java
+++ src/SudokuSolutionVerifier/BasicChecks.java
@@ -1,40 +1,4 @@
 package SudokuSolutionVerifier;
 
-public class dokuVerifier {
-
-    public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Usage: java SudokuVerifier <mode> <csv>");
-            return;
-        }
-
-        int mode;
-        try {
-            mode = Integer.parseInt(args[0]);
-        } catch (Exception e) {
-            System.out.println("Invalid mode");
-            return;
-        }
-
-        String file = args[1];
-
-        int[][] board;
-        try {
-            board = CSVReader.load(file);
-        } catch (Exception e) {
-            System.out.println("Error reading file");
-            return;
-        }
-
-        SudokuMode m;
-        try {
-            m = ModesFactory.create(mode);
-        } catch (Exception e) {
-            System.out.println("Invalid mode");
-            return;
-        }
-
-        ValidationResult r = m.verify(board);
-        ResultPrinter.get().print(r);
-    }
+public class BasicChecks {
 }
